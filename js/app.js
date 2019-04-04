eea9cfd1fa91c79110c5d3d39aa6af2d57defecf
unknown <cecilephilippe31@gmail.com>
2019-04-04 21:17:35 +0200

    carré propriete js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 window.onload = function () {
   console.log("Lancement écran de veille");
-
+  document.querySelector('.carre').style.left="0";
+  document.querySelector('.carre').style.bottom="0";
+console.log("le Dom est chargé");
 }
