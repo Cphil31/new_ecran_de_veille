07f263cf3806c68a7455b9e84f0ec3ae1da43b49
unknown <cecilephilippe31@gmail.com>
2019-04-04 22:22:46 +0200

    partie 1 : 4 ok

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 window.onload = function () {
+  document.querySelector('#carre').style.left=`0px`;
+  document.querySelector('#carre').style.bottom=`0px`;
 
 
-  let x =+ prompt("Deffinissez x");
-  let y =+ prompt("Deffinissez y");
-  let a = document.querySelector('#carre').style.left=`${x}px`;
-  let b = document.querySelector('#carre').style.bottom=`${y}px`;
+  document.getElementById("go").addEventListener("click", function(){
+    let x =+ prompt("Deffinissez x");
+    let y =+ prompt("Deffinissez y");
+    let a = document.querySelector('#carre').style.left=`${x}px`;
+    let b = document.querySelector('#carre').style.bottom=`${y}px`;
 
-
+  });
 
 }
