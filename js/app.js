ece8b5c234aeda5815aec7e411aadcc77501cad4
unknown <cecilephilippe31@gmail.com>
2019-04-04 22:29:08 +0200

    partie 1 : 5 ok

--- js/app.js
+++ js/app.js
@@ -2,6 +2,20 @@ window.onload = function () {
   document.querySelector('#carre').style.left=`0px`;
   document.querySelector('#carre').style.bottom=`0px`;
 
+  const min = 0;
+  const max = 600;
+
+  let  ckeckValue = (x) => {
+    if (x>=min || x<max){
+      return true;
+    }
+    else{
+      return false ;
+    }
+  };
+
+
+
 
   document.getElementById("go").addEventListener("click", function(){
     let x =+ prompt("Deffinissez x");
