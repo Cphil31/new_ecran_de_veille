017b8635985b2757fdab8447976c0453d58bf2ae
unknown <cecilephilippe31@gmail.com>
2019-04-05 00:04:58 +0200

    partie 1 : 8 en cours

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ window.onload = function () {
 
   const min = 0;
   const max = 600;
-  const cycle = 1000;
+  const cycle = 1;
+  let set ;
 
   let  checkValue = (a) => {
     if (a>=min || a<=max){
@@ -16,23 +17,46 @@ window.onload = function () {
   };
 
 
+  let changePositionX = (x) =>{
+    document.querySelector('#carre').style.left=`${x}px`;
+  }
+  let changePositionY = (y) =>{
+    document.querySelector('#carre').style.bottom=`${y}px`;
+  }
+  let run = () =>{
+
+  }
+
   document.getElementById("go").addEventListener("click", function(){
     let x = prompt("Deffinissez x");
     let y = prompt("Deffinissez y");
+
     if  (x>=min && x<=max && y>=min && y<=max ) {
       document.querySelector('#carre').style.left=`${x}px`;
       document.querySelector('#carre').style.bottom=`${y}px`;
 
-      setInterval(function run (){
-        console.log(x);
-        console.log(y);
-        }, cycle);
+      let set = setInterval(
+        function run (){
+          x++;
+          y++;
+          changePositionX(x);
+          changePositionY(y);
+          console.log(x);
+          console.log(y);
+
+      }
+      , cycle);
 
     }
     else{
       console.log(`%cles valeurs x et y entrées doivent etre comprises entre ${x} et ${y}`,'background-color:red;');
     }
   });
+
+  document.getElementById("stop").addEventListener("click", function(){
+     clearInterval(set);
+  });
+
 
 
 
