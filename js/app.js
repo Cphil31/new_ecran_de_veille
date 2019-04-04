62cc105438967531f2360024eff6ee9f23c0c41f
unknown <cecilephilippe31@gmail.com>
2019-04-04 22:15:23 +0200

    partie 1 : 3 ok

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 window.onload = function () {
-  console.log("Lancement écran de veille");
-  document.querySelector('.carre').style.left="0";
-  document.querySelector('.carre').style.bottom="0";
-console.log("le Dom est chargé");
+
+
+  let x =+ prompt("Deffinissez x");
+  let y =+ prompt("Deffinissez y");
+  let a = document.querySelector('#carre').style.left=`${x}px`;
+  let b = document.querySelector('#carre').style.bottom=`${y}px`;
+
+
+
 }
